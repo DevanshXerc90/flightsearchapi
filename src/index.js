bffeda2e304cba54465322cfee9a3166ee34759e
cyberhunk49 <23bcs074.iiitdmj.ac.in>
2024-12-21 20:39:04 +0530

    added filters to get all cities

--- src/index.js
+++ src/index.js
@@ -1,19 +1,25 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const { PORT } = require('./config/serverConfig');
+const ApiRoutes = require('./routes/index');
 
-const setupAndStartServer = async() => {
 
-// create the express object
 
-const app = express();
+const setupAndStartServer = async () => {
 
-app.listen(PORT, () => {
+    // create the express object
+    const app = express();
 
-console.log(`Server started at ${PORT}`);
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({extended: true}));
 
-});
+    app.use('/api', ApiRoutes);
 
+    app.listen(PORT, async () => {
+        console.log(`Server started at ${PORT}`);
+        
+    });
 }
 
 setupAndStartServer();
--- src/services/index.js
+++ src/services/index.js
@@ -1,6 +1,4 @@
 module.exports = {
-    CityService: require('./city-service'),
-    FlightService: require('./flight-service'),
-    AirportService: require('./airport-service'),
-    CrudService: require('./crud-service')
+    CityService: require('./city-service')
+    
 }
